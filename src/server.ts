05498f205de105167b66b912c23b5aa3315fcd3f
Vasant18 <vasantspujar018@gmail.com>
2024-11-29 16:25:44 +0530

    modified server.ts file.

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,5 @@
 // src/server.ts
-import { DefaultService } from "../node-client";
+import { DefaultService } from "../node-client"; //in the client just install the client package and import it and use  it.
 import { app } from "./app";
 const port = process.env.PORT || 3000;
 
@@ -7,4 +7,4 @@ app.listen(port, () =>
   console.log(`Example app listening at http://localhost:${port}`)
 );
 
-DefaultService.getTodo("asd");
+DefaultService.getTodo("asd"); // instead of using  fetch  just use the import client and use the method directly
