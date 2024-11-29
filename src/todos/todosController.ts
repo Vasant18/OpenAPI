15b77af4235d201897b2e43a854f5593e25c9347
Vasant18 <vasantspujar018@gmail.com>
2024-11-29 20:57:12 +0530

    updated some files.

--- src/todos/todosController.ts
+++ src/todos/todosController.ts
@@ -1,17 +1,21 @@
 // src/users/usersController.ts
 import {
-    Controller,
-    Get,
-    Path,
-    Route
+  Controller,
+  Get,
+  Path,
+  Route
 } from "tsoa";
+import { Todo } from './todo';
 import { TodoService } from "./todoService";
   @Route("todo")
   export class TodoController extends Controller {
+    /**
+     * This is the controller for creating todos
+     */
    @Get("{todoId}")
    public async getTodo (
     @Path() todoId : string
-   ){
+   ): Promise<Todo> {
     let todoService = new TodoService();
     return todoService.get(todoId);         
    }
